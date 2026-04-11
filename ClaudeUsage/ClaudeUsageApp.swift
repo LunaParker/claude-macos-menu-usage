81225d06e0f7bccb030cb7cede92dc1004e86897
Luna Parker <lunaparker33@gmail.com>
2026-04-11 13:33:23 -0400

    Initial Commit

---
+++ ClaudeUsage/ClaudeUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageApp.swift
+//  ClaudeUsage
+//
+//  Created by Luna Parker on 4/11/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
